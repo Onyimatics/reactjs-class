090166e4cff3f73f667bf7a2c7eb4705a84c9c78
Onyimatics <ezikeonyinyefavour@gmail.com>
2020-11-25 17:57:14 +0100

    react event emitters

--- src/index.js
+++ src/index.js
@@ -53,13 +53,31 @@ const Book = ({ img, title, author }) => {
     // const Book = (props) => {
     // destructuring props
     // const { img, title, author } = props;
+    //attribute, eventHandler
+    //onClick, onMouseHover
+    const clickHandler = (e) => {
+        console.log(e)
+        console.log(e.target)
+        alert('hello world')
+    }
+    const complexExample = (author) => {
+        console.log(author)
+    }
     return (
-        <article className={'book'}>
+        <article className={'book'} onMouseOver={() => {
+            console.log(title)
+        }}>
             <img
                 src={img}
                 alt={''}/>
-            <h1>{title}</h1>
+            <h1 onClick={() => console.log(title)}>{title}</h1>
             <h4>{author}</h4>
+            <button type={"button"} onClick={clickHandler} style={{cursor: 'pointer'}}>
+                Reference Example
+            </button>
+            <button type={"button"} onClick={() => complexExample(author)}>
+                More Complex Example
+            </button>
         </article>
     )
 }
