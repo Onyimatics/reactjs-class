73cd769a4bb48536689f6b853bc156ebbfad8685
ONYIMATICS_MAC <user@ONYIMATICSs-MacBook-Pro.local>
2020-11-25 10:11:37 +0100

    react props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*function Greeting() {
+    return (
+        <React.Fragment>
+            <div>
+                {/!*<h4>John Doe</h4>*!/}
+                <Person />
+                <Message />
+            </div>
+        </React.Fragment>);
+}*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*const Greeting = () => {
+    return React.createElement('div', {},
+        React.createElement('h1', {}, 'Hello World'));
+}*/
+
+const BookList = () => {
+    return (
+        <section className='bookList'>
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+            <Book img={secondtBook.img} title={secondtBook.title} author={secondtBook.author}/>
+        </section>
+    )
+}
+
+const firstBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth'
+}
+
+const secondtBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81GIvo3b02L._AC_UL200_SR200,200_.jpg',
+    title: 'Paint by sticker',
+    author: 'Workman Publishing'
+}
+
+const Book = ({ img, title, author }) => {
+    // const Book = (props) => {
+    // destructuring props
+    // const { img, title, author } = props;
+    return (
+        <article className={'book'}>
+            <img
+                src={img}
+                alt={''}/>
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+        </article>
+    )
+}
+
+/*const Book = (props) => {
+    console.log(props)
+    return (
+        <article className={'book'}>
+            <img
+                src={props.img}
+                alt={''}/>
+            <h1>{props.title}</h1>
+            <h4>{props.author}</h4>
+        </article>
+    )
+}*/
+
+/*const Book = () => {
+    return (
+        <article className={'book'}>
+            <Image></Image>
+            <Title />
+            <Author />
+        </article>
+    )
+}
+
+const Image = () => {
+    return <img
+        src={'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'}
+        alt={'Book'}/>
+}
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>Amelia Hepworth</h4>*/
+
+/*const Person = () => <h4>John Doe</h4>
+const Message = () => {
+    return <p>This is my message</p>
+}*/
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
